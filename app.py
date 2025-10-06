cbd6cc84aa75cd46773efedf90ea2e1b561cf75e
fidhafathimakp <fidhafathimakp681@gmail.com>
2025-10-06 19:40:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import plotly.express as px
 from datetime import datetime
 
 # ---- Database connection settings ----
-DB_HOST = "your_host"
+DB_HOST = "db.pbollegtlkcxdbylblna.supabase.co"
 DB_PORT = "5432"
-DB_NAME = "your_db"
-DB_USER = "your_user"
-DB_PASS = "your_password"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "5kdWkrxe9QXZOVNw"
 
 def get_conn():
     return psycopg2.connect(host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASS)
